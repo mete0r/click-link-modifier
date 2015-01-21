5813b234742e62302f95d1a8e3ebb7efaaddac62
mete0r <mete0r@sarangbang.or.kr>
2015-01-21 10:10:20 +0900

    initial

---
+++ data/contentscript.js
@@ -0,0 +1,29 @@
+const options = self.options;
+console.debug('contentscript: loaded', options);
+window.addEventListener("click", function(e) {
+  console.debug('contentscript: document got click event');
+  if (e.target instanceof Element) {
+    for (let elem = e.target;
+         elem != null;
+         elem = elem.parentElement) {
+      if (elem instanceof HTMLAnchorElement) {
+        console.debug('contentscript: AnchorElement');
+        let combination = [
+          e.shiftKey? 'S': '-',
+          e.ctrlKey ? 'C': '-',
+          e.altKey  ? 'A': '-',
+          e.metaKey ? 'M': '-'
+        ].join('');
+        let action = options.actionsForModifierCombination[combination];
+        if (action) {
+          self.port.emit(action, elem.href);
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }
+    }
+  }
+},
+false, /* don't capture events*/
+false  /* discard untrusted events, i.e. synthetic events dispatched by web content */
+);
